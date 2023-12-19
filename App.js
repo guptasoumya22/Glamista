30a815a7fbccc6dd7923f81af2333dd94aee40e9
Soumya Gupta <soumyagupta1022@gmail.com>
2023-12-19 13:34:04 +0530

    Add files via upload
    Made home,Login,Signup page and their routing

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Home from './pages/Home';
+import {Counter} from './features/counter/Counter'
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import { createRoot } from "react-dom/client";
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
